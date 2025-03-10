715eb926bd96c47e6494fdeace3fcfe46bbceddd
Redietgc-27 <redietbeharu45@gmail.com>
2025-03-11 01:52:25 +0300

    Fixed Router basename issue for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { DataProvider } from "./Components/DataProvider/DataProvider.jsx";
 import { reducer, initialState } from "./Utility/reducer.js";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <DataProvider reducer={reducer} initialState={initialState}>
-      <App />
-    </DataProvider>
+    <BrowserRouter basename="/Amazon-clone-2025">
+      <DataProvider reducer={reducer} initialState={initialState}>
+        <App />
+      </DataProvider>
+    </BrowserRouter>
   </StrictMode>
 );
