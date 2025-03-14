6489416b2d4e642890118a13613d9af18c2136a5
Redietgc-27 <redietbeharu45@gmail.com>
2025-03-14 21:47:13 +0300

    axios

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+// const axiosInstance = axios.create({
+//   baseURL: "http://localhost:5000",
 
-  // });
-  // const axiosInstance = axios.create({
-  //   baseURL: "https://amazon-api-deploy-fhu3.onrender.com/",
+// });
+const axiosInstance = axios.create({
+  baseURL: "https://amazon-api-deploy-fhu3.onrender.com/",
 });
 export { axiosInstance };
